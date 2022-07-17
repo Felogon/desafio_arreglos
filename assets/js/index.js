fa80fd826dff28e1b7a3960c2ff0deb2ed1e4530
Felogon <fe.gonzaleze@gmail.com>
2022-07-16 20:37:11 -0400

    js modificado

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,114 +1,146 @@
+// Arreglo
 const propiedadesJSON = [
-    {
-      name: "Casa de campo",
-      description: "Un lugar ideal para descansar de la ciudad",
-      src:
-        "https://www.construyehogar.com/wp-content/uploads/2020/02/Dise%C3%B1o-casa-en-ladera.jpg",
-      rooms: 2,
-      m: 170
-    },
-    {
-      name: "Casa de playa",
-      description: "Despierta tus días oyendo el oceano",
-      src:
-        "https://media.chvnoticias.cl/2018/12/casas-en-la-playa-en-yucatan-2712.jpg",
-      rooms: 2,
-      m: 130
-    },
-    {
-      name: "Casa en el centro",
-      description: "Ten cerca de ti todo lo que necesitas",
-      src:
-        "https://fotos.perfil.com/2018/09/21/trim/950/534/nueva-york-09212018-366965.jpg",
-      rooms: 1,
-      m: 80
-    },
-    {
-      name: "Casa rodante",
-      description: "Conviertete en un nómada del mundo sin salir de tu casa",
-      src:
-        "https://cdn.bioguia.com/embed/3d0fb0142790e6b90664042cbafcb1581427139/furgoneta.jpg",
-      rooms: 1,
-      m: 6
-    },
-    {
-      name: "Departamento",
-      description: "Desde las alturas todo se ve mejor",
-      src:
-        "https://www.adondevivir.com/noticias/wp-content/uploads/2016/08/depto-1024x546.jpg",
-      rooms: 3,
-      m: 200
-    },
-    {
-      name: "Mansión",
-      description: "Vive una vida lujosa en la mansión de tus sueños ",
-      src:
-        "https://resizer.glanacion.com/resizer/fhK-tSVag_8UGJjPMgWrspslPoU=/768x0/filters:quality(80)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/CUXVMXQE4JD5XIXX4X3PDZAVMY.jpg",
-      rooms: 5,
-      m: 500
-    }
-  ];
-  
-function templateDepartamento(propiedad){
-  return  `<div class="propiedad">
-          <div
-            class="img"
-            style="
-              background-image: url('https://resizer.glanacion.com/resizer/fhK-tSVag_8UGJjPMgWrspslPoU=/768x0/filters:quality(80)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/CUXVMXQE4JD5XIXX4X3PDZAVMY.jpg');
-            "
-          ></div>
-          <section class="section-1">
-            <h5>${propiedad.titulo}</h5>
-            <div class="d-flex justify-content-between">
-              <p>Cuartos: 2</p>
-              <p>Metros: 170</p>
-            </div>
-            <p class="my-3">Mansión gigante</p>
-            <button class="btn btn-info">Ver más</button>
-          </section>
-        </div>`
-}
+  {
+    nombre: "Casa de campo",
+    descripcion: "Un lugar ideal para descansar de la ciudad",
+    src: "https://www.construyehogar.com/wp-content/uploads/2020/02/Dise%C3%B1o-casa-en-ladera.jpg",
+    cuartos: 2,
+    metros: 170,
+  },
+  {
+    nombre: "Casa de playa",
+    descripcion: "Despierta tus días oyendo el oceano",
+    src: "https://media.chvnoticias.cl/2018/12/casas-en-la-playa-en-yucatan-2712.jpg",
+    cuartos: 2,
+    metros: 130,
+  },
+  {
+    nombre: "Casa en el centro",
+    descripcion: "Ten cerca de ti todo lo que necesitas",
+    src: "https://fotos.perfil.com/2018/09/21/trim/950/534/nueva-york-09212018-366965.jpg",
+    cuartos: 1,
+    metros: 80,
+  },
+  {
+    nombre: "Casa rodante",
+    descripcion: "Conviertete en un nómada del mundo sin salir de tu casa",
+    src: "https://cdn.bioguia.com/embed/3d0fb0142790e6b90664042cbafcb1581427139/furgoneta.jpg",
+    cuartos: 1,
+    metros: 6,
+  },
+  {
+    nombre: "Departamento",
+    descripcion: "Desde las alturas todo se ve mejor",
+    src: "https://www.adondevivir.com/noticias/wp-content/uploads/2016/08/depto-1024x546.jpg",
+    cuartos: 3,
+    metros: 200,
+  },
+  {
+    nombre: "Mansión",
+    descripcion: "Vive una vida lujosa en la mansión de tus sueños ",
+    src: "https://thumbs.dreamstime.com/b/jacobean-manor-house-15201867.jpg",
+    cuartos: 20,
+    metros: 500,
+  },
+  {
+    nombre: "Loft",
+    descripcion: "Amplio espacio en un entorno que invita a la creatividad ",
+    src: "https://images.hola.com/imagenes/decoracion/20210621191783/loft-estilo-eclectico-industrial-decoracion-interiores-lm/0-966-599/loft-antrax-1a-a.jpg",
+    cuartos: 2,
+    metros: 120,
+  },
+  {
+    nombre: "Ley Pereira",
+    descripcion:
+      "Para los que gustan de un estilo funcional, con  buen gusto y un toque inglés",
+    src: "https://cl.habcdn.com/photos/project/medium/10-300602.jpg",
+    cuartos: 4,
+    metros: 200,
+  },
+];
 
-function cargaInicial(){
-  let html = "";
-  for (const depto of propiedadesJSON){
-    // Si cumplen los requerimientos mostramos el depto
-    if (
-      /* cantidad de cuartos*/
-      /* cantidad m2*/
-    ){
-      // agregar depto a la lista 
-    html += templateDepartamento(depto)
-    }
-  
-    innerHTML = html;
-}
-
-window.document.onload = function(){
-  cargaInicial();
-}
-
-
-function buscar (){
-  if (/* los campos estan vacios*/) {
-    alert ()
-    return;
-  }
-}
-
+// Declaracion variables
 let html = "";
-let total = 0;
+let propSeleccionada;
+let total;
 
-// Recorrer deptos
-for (const depto of propiedadesJSON){
-  // Si cumplen los requerimientos mostramos el depto
-  if (
-    /* cantidad de cuartos*/
-    /* cantidad m2*/
-  ){
-    // agregar depto a la lista 
-  html += templateDepartamento(depto)
+//let total;
+cajaPropiedades = document.querySelector(".propiedades");
+let cantidad = document.querySelector(".py-3");
+
+cargaInicial();
+
+// Carga Inicial
+function cargaInicial() {
+  for (let propiedad of propiedadesJSON) {
+    html += `<div class="propiedad">
+            <div class="img" style="background-image: url(${propiedad.src})"></div>
+                <section>
+                    <h5>${propiedad.nombre}</h5>
+                    <div class="d-flex justify-content-between">
+                        <p>Cuartos: ${propiedad.cuartos}</p>
+                        <p>Metros: ${propiedad.metros}</p>
+                    </div>
+                    <p class="my-3">${propiedad.descripcion}</p>
+                    <button class="btn btn-info ">Ver más</button>
+                </section>
+         </div>`;
   }
 
-  innerHTML = html;
+  cajaPropiedades.innerHTML = html;
+}
+
+// Evento onClick
+function buscarPropiedad() {
+  html = "";
+  let total;
+  let cuartos = Number(document.querySelector("#inputCuartos").value);
+  let desde = Number(document.querySelector("#inputDesde").value);
+  let hasta = Number(document.querySelector("#inputHasta").value);
+
+  if (cuartos == 0 || desde == 0 || hasta == 0) {
+    alert("Todos los campos de filtro deben tener datos");
+  } else {
+    propSeleccionada = propiedadesJSON.filter((propiedadSel) => {
+      return (
+        propiedadSel.cuartos >= cuartos &&
+        propiedadSel.metros >= desde &&
+        propiedadSel.metros <= hasta
+      );
+    });
+
+    for (let propiedadFiltrada of propSeleccionada) {
+      template(propiedadFiltrada);
+    }
+    console.log("propSeleccionada->", propSeleccionada.length);
+
+    total = propSeleccionada.length;
+    cantidad.innerHTML = "Propiedades encontradas: " + total;
+    console.log(total);
+  }
+
+  if (total == 0) {
+    cajaPropiedades.innerHTML =
+      '<div><h1> Intenta buscar nuevamente <i class="bi bi-search"></i></h1></div>';
+    cantidad.innerHTML = 0;
+    alert("No hay propiedades que cumplan con los criterios de busqueda");
+  }
+}
+
+// Template
+function template(propiedadFiltrada) {
+  html += `<div class="propiedad">
+              <div class="img" style="background-image: url(${propiedadFiltrada.src})"></div>
+              <section>
+                  <h5>${propiedadFiltrada.nombre}</h5>
+                  <div class="d-flex justify-content-between">
+                      <p>Cuartos: ${propiedadFiltrada.cuartos}</p>
+                      <p>Metros: ${propiedadFiltrada.metros}</p>
+                  </div>
+                  <p class="my-3">${propiedadFiltrada.descripcion}</p>
+                  <button class="btn btn-info ">Ver más</button>
+              </section>
+          </div>`;
+
+  cajaPropiedades.innerHTML = html;
 }
